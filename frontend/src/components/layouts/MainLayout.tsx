66176728b6feddcf472e64c4b470c1b268064323
23120320 <23120320@student.hcmus.edu.vn>
2025-12-02 20:43:08 +0700

    refactor: switch layout rendering from children to Outlet

--- frontend/src/components/layouts/MainLayout.tsx
+++ frontend/src/components/layouts/MainLayout.tsx
@@ -1,20 +1,18 @@
 import React from "react";
 import Header from "../common/Header";
 import Footer from "../common/Footer";
-
-// --- 1. DEFINE TYPESCRIPT FOR PROPS ---
-interface MainLayoutProps {
-  children: React.ReactNode;
-}
+import { Outlet } from "react-router-dom";
 
 // --- 2. COMPONENT MAINLAYOUT ---
-const MainLayout: React.FC<MainLayoutProps> = ({ children }) => {
+const MainLayout: React.FC = () => {
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
 
       <main className="flex-grow bg-[#ececec] pt-2 md:pt-0 w-full">
-        <div className="max-w-[1200px] mx-auto px-4 sm:px-6">{children}</div>
+        <div className="max-w-[1200px] mx-auto px-4 sm:px-6">
+          <Outlet />
+        </div>
       </main>
 
       <Footer />
